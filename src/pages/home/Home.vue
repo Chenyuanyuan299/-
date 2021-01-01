6f555bc246ae4ae106a97dc18f1329d2737fc735
Chenyuanyuan299 <1872764995@qq.com>
2021-01-01 18:00:20 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<!--template根下只能有一个标签，所以外层用div进行包裹-->
+<template>
+  <div>
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
